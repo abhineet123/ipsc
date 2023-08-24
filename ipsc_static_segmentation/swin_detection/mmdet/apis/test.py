bde59689c6155ca82492173de54a69f0914fb3fa
abhineet123 <asingh1@ualberta.ca>
2023-08-24 05:41:10 +0100

    minor fix

--- ipsc_static_segmentation/swin_detection/mmdet/apis/test.py
+++ ipsc_static_segmentation/swin_detection/mmdet/apis/test.py
@@ -146,7 +146,7 @@ def single_gpu_test(model,
             csv_rows = model.module.show_result(
                 img_show,
                 curr_result,
-                show=1,
+                show=False,
                 out_dir=seq_out_dir,
                 out_filename=img_name,
                 out_xml_dir=seq_xml_out_dir,
